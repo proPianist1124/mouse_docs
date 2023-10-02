260ea91121420d4ca70ce2d46682f4a81cb968f6
proPianist1124 <phantomforces11242009@gmail.com>
2023-10-01 18:17:28 -0700

    mw fix

--- src/middleware.ts
+++ src/middleware.ts
@@ -5,14 +5,11 @@ import { get_user } from "./pages/api/data"
 // Custom middleware for protection
 export async function middleware(req: NextRequest) {
     let user:any = req.cookies.get("sid")
-    try{
-        if(await get_user(user.value)){
-            return NextResponse.next()
-        }else{
-            return NextResponse.next()
-        }
-    }catch(e){
+    if(user == undefined){
         return NextResponse.rewrite(new URL("/auth", req.url))
+    }else{
+        console.log(user.value)
+        return NextResponse.next()
     }
 }
 
